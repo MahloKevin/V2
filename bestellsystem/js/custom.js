cb5fb2527a9aa942bf42e5c0b81cb746a091742c
MahloKevin <kevin.seitz@mahlo.com>
2024-05-06 14:07:54 +0200

    Added many things

--- bestellsystem/js/custom.js
+++ bestellsystem/js/custom.js
@@ -1,14 +1,30 @@
-// to get current year
+//Spielt das aktuelle Jahr als Copyright ein
+getYear();
+
+//Funktion um Passwort einzusehen
+document.getElementById("togglePassword").addEventListener("click", togglePassword);
+
+function togglePassword() {
+    var passwordField = document.getElementById('password');
+    var lockIcon = document.getElementById('togglePassword');
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        lockIcon.className = "fa-solid fa-lock-open";
+    } else {
+        passwordField.type = "password";
+        lockIcon.className = "fa-solid fa-lock";
+    }
+}
+
+
+// Aktuelles Jahr bekommen
 function getYear() {
     var currentDate = new Date();
     var currentYear = currentDate.getFullYear();
     document.querySelector("#displayYear").innerHTML = currentYear;
 }
 
-getYear();
-
-
-// isotope js
+// isotope js (aka. Filter System)
 $(window).on('load', function () {
     $('.filters_menu li').click(function () {
         $('.filters_menu li').removeClass('active');
@@ -28,13 +44,3 @@ $(window).on('load', function () {
         }
     })
 });
-
-
-/** google_map js **/
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(48.903090612447684, 11.929230150181501),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
