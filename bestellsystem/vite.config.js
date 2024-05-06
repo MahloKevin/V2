023084ae613a585d7cbbe99441180d20948763d3
MahloKevin <kevin.seitz@mahlo.com>
2024-05-06 08:37:06 +0200

    Apparently it did work, i don't even know how

--- bestellsystem/vite.config.js
+++ bestellsystem/vite.config.js
@@ -1,17 +1,7 @@
-import {defineConfig, resolveConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'about.html'),
-        menu: resolve(__dirname, 'menu.html'),
-        test: resolve(__dirname, 'test.html'),
-      }
-    }
-  }
 })
