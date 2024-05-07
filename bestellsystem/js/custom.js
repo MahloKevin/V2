1dff59bf202c932ee309ee6e10c7388328c664be
MahloKevin <kevin.seitz@mahlo.com>
2024-05-07 07:43:58 +0200

    Fixed the maps part again

--- bestellsystem/js/custom.js
+++ bestellsystem/js/custom.js
@@ -1,6 +1,9 @@
 //Spielt das aktuelle Jahr als Copyright ein
 getYear();
 
+//Fügt Google Maps auf der Startseite ein
+myMap()
+
 //Funktion um Passwort einzusehen
 document.getElementById("togglePassword").addEventListener("click", togglePassword);
 
@@ -22,4 +25,13 @@ function getYear() {
     var currentDate = new Date();
     var currentYear = currentDate.getFullYear();
     document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+/** google_map js **/
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(48.90302550090018, 11.929094617806973),
+        zoom: 18,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
