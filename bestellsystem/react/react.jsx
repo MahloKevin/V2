bbcbc734dc365b754c96a615753689df398d92c6
azu-ks <kevin.seitz@mahlo.com>
2024-05-08 08:42:34 +0200

    Trying to fix my react stuff

--- bestellsystem/react/react.jsx
+++ bestellsystem/react/react.jsx
@@ -2,6 +2,6 @@ import { createRoot } from 'react-dom/client';
 import Header from "./components/Header.jsx";
 
 
-const domNode = document.getElementById('navigation');
+const domNode = document.getElementById('header');
 const root = createRoot(domNode);
 root.render(<Header />);
